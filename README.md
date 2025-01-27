a4e4cf65cb47dabd1bf6aba8c26d5cacab5ac822
SunnyGitGit <sunyoung4757@gmail.com>
2025-01-27 14:45:01 +0000

    added new endpoints of GET /api

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Northcoders News API
 
-For instructions, please head over to [L2C NC News](https://l2c.northcoders.com/courses/be/nc-news).
+## Project Setup
+
+### Making a public repo
+- to make your own public repo and push your code to the repo, use the following commands,
+    git remote set-url origin YOUR_NEW_REPO_URL_HERE
+    git branch -M main
+    git push -u origin main
+
+### Creating the databases and setting up environment variables
+- to create two databases in this project, create two .env files: .env.test and .env. development
+(Double check that these .env files are .gitignored)
+
+
 
 
 
---
+++ errors/errors.js
@@ -0,0 +1,21 @@
+exports.handlePSQLErrors = (err, req, res, next) => {
+    if (err.code === "22P02" || err.code === "23502") {
+        res.status(400).send({ msg: "Bad request" });
+    } else next(err);
+};
+
+exports.handleReferenceErrors = (err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(400).send({ msg: "Not Found" });
+    } else next(err);
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+};
+  
+exports.handleServerErrors = (err, req, res, next) => {
+    res.status(500).send({ msg: "Internal Server Error" })
+};  
