e6186cf86e75c4b152dff8b05372f67f59bc0db3
SunnyGitGit <sunyoung4757@gmail.com>
2025-01-28 11:03:46 +0000

    added a new endpoint of GET /api/articles/:article_id to get an article by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,3 +46,42 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responses with the correct article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        const { article } = response.body;
+
+        expect(article).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("404: Responses with an error if article is not found", () => {
+    return request(app)
+      .get("/api/articles/999999")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Not Found");
+      });
+  });
+  test("400: Responses with an error if article_id is not a number", () => {
+    return request(app)
+      .get("/api/articles/dog")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad Request");
+      });
+  });
+});
--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -1,9 +1,23 @@
-const { fetchTopics } = require("../models/topics-models");
+const { fetchTopics,
+        fetchArticleById 
+    } = require("../models/topics-models");
 
 exports.getTopics = (req, res, next) => {
     fetchTopics()
     .then((topics) => {
         res.status(200).send({ topics });
+    })
+    .catch((err) => {
+        next(err);
+    });
+};
+
+exports.getArticleById = (req, res, next) => {
+    const { article_id } = req.params;
+
+    fetchArticleById(article_id)
+    .then((article) => {
+        res.status(200).send({ article });
     })
     .catch((err) => {
         next(err);
