c75d112f65a6b44750afb8c2a4ab7d378ce094d4
SunnyGitGit <sunyoung4757@gmail.com>
2025-01-31 14:27:45 +0000

    updated set up a database instance using Supabase

--- db/connection.js
+++ db/connection.js
@@ -7,8 +7,9 @@ require('dotenv').config({
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+};
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
