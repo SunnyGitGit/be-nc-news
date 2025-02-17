648bcb29763dfa463c0695bc0edeb46ac056931a
SunnyGitGit <sunyoung4757@gmail.com>
2025-02-17 22:10:21 +0000

    updated listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("./app");
-const { PORT = 9090 } = process.env;
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
